cc563ac5a12cd5e067f6e44ae6913ddde91ad2ab
Ryan Sproule <rsproule23@gmail.com>
2025-08-12 17:44:44 -0400

    handle non-json error results

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,9 +1,9 @@
 import {
   BadRequestError,
   InternalServerError,
+  MeritError,
   NotFoundError,
   UnauthorizedError,
-  MeritError,
   type APIResponse,
 } from '../types.js';
 
@@ -27,39 +27,74 @@ export abstract class BaseAPI {
         },
       });
 
-      const data = await response.json();
+      try {
+        const data = await response.json();
 
-      if (!response.ok) {
-        switch (data.status) {
+        if (!response.ok) {
+          // we have an structured json error from the server
+          switch (data.status) {
+            case 400:
+              throw new BadRequestError({
+                status: response.status,
+                message: data.message ?? 'Unknown error occurred',
+                request_id: data.request_id,
+              });
+            case 401:
+              throw new UnauthorizedError({
+                status: response.status,
+                message: data.message ?? 'Unknown error occurred',
+                request_id: data.request_id,
+              });
+            case 404:
+              throw new NotFoundError({
+                status: response.status,
+                message: data.message ?? 'Unknown error occurred',
+                request_id: data.request_id,
+              });
+            case 500:
+              throw new InternalServerError({
+                status: response.status,
+                message: data.message ?? 'Unknown error occurred',
+                request_id: data.request_id,
+              });
+            default:
+              throw new MeritError({
+                status: response.status,
+                message: data.message ?? 'Unknown error occurred',
+                request_id: data.request_id,
+              });
+          }
+        }
+        return data;
+      } catch {
+        switch (response.status) {
           case 400:
             throw new BadRequestError({
               status: response.status,
-              message: data.message ?? 'Unknown error occurred',
+              message: response.statusText ?? 'Unknown error occurred',
             });
           case 401:
             throw new UnauthorizedError({
               status: response.status,
-              message: data.message ?? 'Unknown error occurred',
+              message: response.statusText ?? 'Unknown error occurred',
             });
           case 404:
             throw new NotFoundError({
               status: response.status,
-              message: data.message ?? 'Unknown error occurred',
+              message: response.statusText ?? 'Unknown error occurred',
             });
           case 500:
             throw new InternalServerError({
               status: response.status,
-              message: data.message ?? 'Unknown error occurred',
+              message: response.statusText ?? 'Unknown error occurred',
             });
           default:
             throw new MeritError({
               status: response.status,
-              message: data.message ?? 'Unknown error occurred',
+              message: response.statusText ?? 'Unknown error occurred',
             });
         }
       }
-
-      return data;
     } catch (error) {
       throw new MeritError({
         status: 0,
